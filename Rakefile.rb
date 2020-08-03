c3e9d45344b71b805a7ec32207f49d227dec7a83
ajvictor <69142654+ajvictor@users.noreply.github.com>
2020-08-03 16:27:53 +0530

    Update Rakefile.rb

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,8 +1,8 @@
 require 'rubygems'
+require ‘rake/testtask’
 
 
-desc 'Execute Buyer features'
-task :buyer_features, [:apk_path] do |_task, args|
-  puts 'Running Test Print Statements'
-  #sh "bundle exec calabash-android run #{args[:apk_path]} -p android features/ -t ~@seller -f html -o reports/buyer/buyer.html"
+task default: "test"
+Rake::TestTask.new do |task|
+ task.pattern = ‘test/*_test.rb’
 end
